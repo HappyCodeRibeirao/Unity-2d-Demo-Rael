58e799ba01e886ac1843a63a3396439853ac356b
RAEL <happycode.ribeiraoproeto@gmail.com>
2018-09-29 12:32:11 -0300

    Initial commit

---
+++ Game 2D/Assets/heroController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heroController : MonoBehaviour {
+
+    public float moveX = 0f;
+
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+
+            transform.Translate(new Vector2(moveX, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector2(-moveX, 0));
+
+        }
+	}
+}
